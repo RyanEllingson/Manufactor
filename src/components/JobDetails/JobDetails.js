cf98f2b51f2d8e985d6bef30914acd88e74578cf
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-04-28 15:30:47 -0500

    can now see the currently active operation of a job, complete one operation and go to the next, and see when a job is complete

--- src/components/JobDetails/JobDetails.js
+++ src/components/JobDetails/JobDetails.js
@@ -25,14 +25,44 @@ const JobDetails = function({ handleBack, jobId }) {
 
     const startJob = function(event) {
         event.preventDefault();
+        Object.assign(operations[0], {started: true});
         db.collection("users").doc(`${user.uid}`).collection("jobs").doc(`${jobId}`).set({
-            started: true
+            started: true,
+            operations: operations
         }, { merge: true })
         .then(renderJob())
         .catch((error) => {
             console.error(error);
         });
     };
+
+    const completeStep = function(event, index) {
+        event.preventDefault();
+        Object.assign(operations[index], {completed: true});
+        Object.assign(operations[index + 1], {started: true});
+        db.collection("users").doc(`${user.uid}`).collection("jobs").doc(`${jobId}`).set({
+            operations: operations,
+            activeStep: index + 1
+        }, { merge: true })
+        .then(renderJob())
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+
+    const completeJob = function(event) {
+        event.preventDefault();
+        Object.assign(operations[operations.length - 1], {completed: true});
+        db.collection("users").doc(`${user.uid}`).collection("jobs").doc(`${jobId}`).set({
+            operations: operations,
+            completed: true,
+            activeStep: -1
+        }, { merge: true })
+        .then(renderJob())
+        .catch((error) => {
+            console.error(error);
+        });
+    }
 
     return (
         <div className="container">
@@ -51,6 +81,7 @@ const JobDetails = function({ handleBack, jobId }) {
                                     <p className="card-text">Product: {currentJob.data().productName}</p>
                                     <p className="card-text">Due date: {moment(currentJob.data().dueDate).format("dddd, MMMM Do YYYY")}</p>
                                     <p className="card-text">Customer: {currentJob.data().customer}</p>
+                                    {currentJob.data().completed ? <p className="card-text font-weight-bold">Job Complete!</p> : ""}
                                 </div>
                                 <div className="col-8">
                                     <div className="card">
@@ -65,7 +96,10 @@ const JobDetails = function({ handleBack, jobId }) {
                                                     index={operations.indexOf(operation)}
                                                     activeStep={currentJob.data().activeStep}
                                                     started={currentJob.data().started}
-                                                    handleStart={(e)=>startJob(e)}
+                                                    handleStart={startJob}
+                                                    length={operations.length}
+                                                    handleCompleteStep={completeStep}
+                                                    handleCompleteJob={completeJob}
                                                 />
                                             })}
                                         </ul>
--- src/components/JobOpCard/JobOpCard.js
+++ src/components/JobOpCard/JobOpCard.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-const JobOpCard = function({ name, index, activeStep, started, handleStart }) {
+const JobOpCard = function({ name, index, activeStep, started, handleStart, length, handleCompleteStep, handleCompleteJob }) {
     return (
         <li className={(index === activeStep && started) ? "list-group-item list-group-item-success" : "list-group-item"}>
             <div className="container">
                 <div className="row">
-                    <div className="col-10 my-auto">
+                    <div className="col-9 my-auto">
                         <span>{name}</span>
                     </div>
-                    <div className="col-2">
-                        {(index === 0 && !started) ? <button className="btn btn-success my-auto" onClick={(e) => handleStart(e)}>Start</button> : ""}
+                    <div className="col-3">
+                        {(index === 0 && !started) ? <button className="btn btn-success" onClick={(e) => handleStart(e)}>Start Job</button> : ""}
+                        {(index === activeStep && started && !(activeStep === length - 1)) ? <button className="btn btn-secondary" onClick={(e)=>handleCompleteStep(e, index)}>Complete Step</button> : ""}
+                        {index === activeStep && started && (activeStep === length - 1) ? <button className="btn btn-primary" onClick={(e)=>{handleCompleteJob(e)}}>Complete Job</button> : ""}
                     </div>
                 </div>
             </div>
