83eecbb524ce59bf0eb9b171b010c800729a61a0
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-04-29 13:29:42 -0500

    can perform a compound query for jobs based on whether they are complete and by what products you want

--- src/components/JobSearchForm/JobSearchForm.js
+++ src/components/JobSearchForm/JobSearchForm.js
@@ -1,28 +1,60 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { FirebaseContext } from "../../firebase/firebase";
 
 const JobSearchForm = function({ setJobList }) {
     const { db, user } = useContext(FirebaseContext);
     const [completed, setCompleted] = useState("any");
+    const [productList, setProductList] = useState([]);
+    const [chosenProducts, setChosenProducts] = useState([]);
+    const [chosenProductIds, setChosenProductIds] = useState([]);
+
+    useEffect(() => {
+        db.collection("users").doc(`${user.uid}`).collection("products").get()
+        .then((querySnapshot) => {
+            const products = [];
+            querySnapshot.forEach((doc) => {
+                products.push(doc);
+            });
+            setProductList(products);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const handleSelect = function(items) {
+        const selection = [];
+        const selectionIds = [];
+        for (let i=0; i<items.length; i++) {
+            selection.push(items[i].value)
+            selectionIds.push(items[i].id);
+        }
+        setChosenProducts(selection);
+        setChosenProductIds(selectionIds);
+        console.log(selection);
+        console.log(selectionIds);
+    }
 
     const handleSubmit = function(event) {
         event.preventDefault();
         const jobsRef = db.collection("users").doc(`${user.uid}`).collection("jobs");
-        let query;
+        let completedQuery;
         switch (completed) {
             case "any":
-                query = jobsRef;
+                completedQuery = jobsRef;
                 break;
             case "completed":
-                query = jobsRef.where("completed", "==", true);
+                completedQuery = jobsRef.where("completed", "==", true);
                 break;
             case "not completed":
-                query = jobsRef.where("completed", "==", false);
+                completedQuery = jobsRef.where("completed", "==", false);
                 break;
             default:
                 break;
         }
-        query.get().then((querySnapshot) => {
+        const productsQuery = completedQuery.where("productId", "in", chosenProductIds);
+        productsQuery.get().then((querySnapshot) => {
             const jobs = [];
             querySnapshot.forEach((doc) => {
                 jobs.push(doc);
@@ -41,13 +73,23 @@ const JobSearchForm = function({ setJobList }) {
             <form>
                 <div className="container my-3">
                     <div className="row">
-                        <div className="col-12">
+                        <div className="col-6">
                             <div className="form-group">
                                 <label>Completed?</label>
                                 <select className="form-control" value={completed} onChange={(e)=>{setCompleted(e.target.value)}}>
                                     <option value="any">any</option>
                                     <option value="completed">completed</option>
                                     <option value="not completed">not completed</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="form-group">
+                                <label>Select products</label>
+                                <select multiple={true} className="form-control" value={chosenProducts} onChange={(e)=>{handleSelect(e.target.selectedOptions)}}>
+                                    {productList.map((product)=>{
+                                        return <option key={product.id} id={product.id} value={product.data().name}>{product.data().name}</option>
+                                    })}
                                 </select>
                             </div>
                         </div>
@@ -58,18 +100,6 @@ const JobSearchForm = function({ setJobList }) {
                         </div>
                     </div>
                 </div>
-                
-                {/* <div class="form-group">
-                    <label for="exampleFormControlSelect2">Example multiple select</label>
-                    <select multiple class="form-control" id="exampleFormControlSelect2">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </div> */}
-                
             </form>
         </div>
     );
